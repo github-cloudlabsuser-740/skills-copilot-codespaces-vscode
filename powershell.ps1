397915aa7928db56df5d02a1b11f7a3f79684236
github-cloudlabsuser-740 <167435314+github-cloudlabsuser-740@users.noreply.github.com>
2024-05-13 16:03:06 +0000

    blah balh

---
+++ powershell.ps1
@@ -0,0 +1,37 @@
+# Login to your Azure account
+Connect-AzAccount
+
+# Set your subscription
+$subscriptionId = "your-subscription-id"
+Select-AzSubscription -SubscriptionId $subscriptionId
+
+# Create a resource group
+$resourceGroupName = "myResourceGroup"
+$location = "westeurope"
+New-AzResourceGroup -Name $resourceGroupName -Location $location
+
+# Create a storage account
+$storageAccountName = "mystorageaccount"
+$skuName = "Standard_LRS"
+New-AzStorageAccount -ResourceGroupName $resourceGroupName `
+    -Name $storageAccountName `
+    -Location $location `
+    -SkuName $skuName# Login to your Azure account
+Connect-AzAccount
+
+# Set your subscription
+$subscriptionId = "your-subscription-id"
+Select-AzSubscription -SubscriptionId $subscriptionId
+
+# Create a resource group
+$resourceGroupName = "myResourceGroup"
+$location = "westeurope"
+New-AzResourceGroup -Name $resourceGroupName -Location $location
+
+# Create a storage account
+$storageAccountName = "mystorageaccount"
+$skuName = "Standard_LRS"
+New-AzStorageAccount -ResourceGroupName $resourceGroupName `
+  -Name $storageAccountName `
+  -Location $location `
+  -SkuName $skuName
